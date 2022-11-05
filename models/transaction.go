83ecb053481ee82a7dc5bc3ab202e94b88fd5d9f
Bagas <mbagas221@gmail.com>
2022-11-05 11:40:35 +0700

    update

--- models/transaction.go
+++ models/transaction.go
@@ -2,7 +2,7 @@ package models
 
 type TransactionUser struct {
 	ID      int    `json:"id" gorm:"primary_key:auto_increment"`
-	Value   int    `json:"value" form:"value" gorm:"type: varchar(255)"`
+	Value   int    `json:"value" form:"value"`
 	Status  string `json:"status" form:"status" gorm:"type: varchar(255)"`
 	AdminID int    `json:"admin_id"`
 	BuyerID int    `json:"buyer_id"`
--- pkg/middleware/uploadFile.go
+++ pkg/middleware/uploadFile.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	dto "backend/dto/result"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -18,13 +20,46 @@ func UploadFile(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
+		if err != nil && r.Method == "PATCH" {
+			ctx := context.WithValue(r.Context(), "dataFile", "false")
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
 		if err != nil {
-			fmt.Println(err)
-			json.NewEncoder(w).Encode("Error Retrieving the File")
+			w.WriteHeader(http.StatusBadRequest)
+			response := dto.ErrorResult{Status: http.StatusBadRequest, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
 			return
 		}
 
 		defer file.Close()
+
+		buff := make([]byte, 512)
+		_, err = file.Read(buff)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			response := dto.ErrorResult{Status: http.StatusInternalServerError, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		filetype := http.DetectContentType(buff)
+		if filetype != "image/jpeg" && filetype != "image/png" {
+			w.WriteHeader(http.StatusBadRequest)
+			response := dto.ErrorResult{Status: http.StatusBadRequest, Message: "The provided file format is not allowed. Please upload a JPEG or PNG image"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		_, err = file.Seek(0, io.SeekStart)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			response := dto.ErrorResult{Status: http.StatusInternalServerError, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
 		const MAX_UPLOAD_SIZE = 10 << 20
 		r.ParseMultipartForm(MAX_UPLOAD_SIZE)
 		if r.ContentLength > MAX_UPLOAD_SIZE {
